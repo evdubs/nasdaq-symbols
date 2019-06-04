3200dc232877fe7e6f2340960e67d3833ac20efe
evdubs <evdubs@gmail.com>
2019-06-03 15:06:08 -1000

    archive downloaded nasdaq files

--- extract-transform-load.sh
+++ extract-transform-load.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 dir=$(dirname "$0")
+today=$(date "+%Y-%m-%d")
+current_year=$(date "+%Y")
 
 racket ${dir}/extract.rkt
 racket ${dir}/transform-load.rkt -p "$1"
+
+7zr a /var/tmp/nasdaq/${current_year}.7z /var/tmp/nasdaq/nasdaqtraded.${today}.txt
