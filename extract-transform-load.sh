6e1a31140d18d75ec6d307e152ea70653be5c4b8
evdubs <1124030+evdubs@users.noreply.github.com>
2022-05-02 15:51:18 -1000

    use '-y' to auto recompile .rkt files

--- extract-transform-load.sh
+++ extract-transform-load.sh
@@ -4,9 +4,9 @@ dir=$(dirname "$0")
 today=$(date "+%Y-%m-%d")
 current_year=$(date "+%Y")
 
-racket ${dir}/extract.rkt
-racket ${dir}/transform-load.rkt -p "$1"
+racket -y ${dir}/extract.rkt
+racket -y ${dir}/transform-load.rkt -p "$1"
 
 7zr a /var/tmp/nasdaq/${current_year}.7z /var/tmp/nasdaq/nasdaqtraded.${today}.txt
 
-racket ${dir}/dump-dolt.rkt -p "$1"
+racket -y ${dir}/dump-dolt.rkt -p "$1"
