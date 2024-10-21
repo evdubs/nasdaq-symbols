61ef7fd915364e2b02fecd3d1b1abee4ea691d14
evdubs <1124030+evdubs@users.noreply.github.com>
2024-10-21 11:27:55 -1000

    nasdaqtrader ftp appears down - try to use the website

--- extract-transform-load.sh
+++ extract-transform-load.sh
@@ -4,7 +4,7 @@ dir=$(dirname "$0")
 today=$(date "+%Y-%m-%d")
 current_year=$(date "+%Y")
 
-racket -y ${dir}/extract.rkt
+racket -y ${dir}/extract.2024-10-21.rkt
 racket -y ${dir}/transform-load.rkt -p "$1"
 
 7zr a /var/tmp/nasdaq/${current_year}.7z /var/tmp/nasdaq/nasdaqtraded.${today}.txt
---
+++ extract.2024-10-21.rkt
@@ -0,0 +1,11 @@
+#lang racket/base
+
+(require gregor
+         net/http-easy
+         threading)
+
+(call-with-output-file* (string-append "/var/tmp/nasdaq/nasdaqtraded." (date->iso8601 (today)) ".txt")
+  (λ (out) (~> (get "https://nasdaqtrader.com/dynamic/SymDir/nasdaqtraded.txt")
+               (response-body _)
+               (write-bytes _ out)))
+  #:exists 'replace)
