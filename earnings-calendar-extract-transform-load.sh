e873750bb06097b0de9ca1f49d89dc092d1f13ae
evdubs <1124030+evdubs@users.noreply.github.com>
2025-06-13 09:21:46 -1000

    fix copy/paste error for folder name

--- earnings-calendar-extract-transform-load.sh
+++ earnings-calendar-extract-transform-load.sh
@@ -7,6 +7,6 @@ current_year=$(date "+%Y")
 racket -y ${dir}/earnings-calendar-extract.rkt
 racket -y ${dir}/earnings-calendar-transform-load.rkt -p "$1"
 
-7zr a /var/tmp/zacks/earnings-calendar/${current_year}.7z /var/tmp/zacks/earnings-calendar/${today}
+7zr a /var/tmp/nasdaq/earnings-calendar/${current_year}.7z /var/tmp/nasdaq/earnings-calendar/${today}
 
 # racket -y ${dir}/dump-dolt-calendar.rkt -p "$1"
